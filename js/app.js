eced44cec757b12a05e604b43d3557689ac23908
robelcse <mdrobel.cse@gmail.com>
2021-09-03 23:56:56 +0600

    home page updated

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let home = document.getElementById("home")
+let contentSection = document.getElementById("content-section")
+home.addEventListener("click", function(){
+
+    contentSection.style.left = '30%'
+})
