900314594c78b4075172787182e30184c1f1666b
robelcse <mdrobel.cse@gmail.com>
2021-09-04 12:47:09 +0600

    index file delted

--- js/app.js
+++
@@ -1,6 +0,0 @@
-let home = document.getElementById("home")
-let contentSection = document.getElementById("content-section")
-home.addEventListener("click", function(){
-
-    contentSection.style.left = '30%'
-})
