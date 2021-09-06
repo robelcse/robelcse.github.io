66fdb1cf1f733b177114c24221a10ec0011c8c77
robelcse <mdrobel.cse@gmail.com>
2021-09-06 13:21:03 +0600

    index file updated

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-console.log("hello worl")
+//select navbar
+let sidebar = document.getElementById("sidebar-section")
+let closebtn = document.querySelector(".fa-close")
+let showbtn = document.querySelector(".fa-bars")
 
-let home = document.getElementById("home")
-let aboutsection = document.querySelector(".about")
-let homeSection = document.querySelector(".home")
- 
-home.addEventListener("click", function(){
+showbtn.style.display = 'block';
+closebtn.style.display = 'none';
+showbtn.addEventListener("click", function(){
 
-    aboutsection.classList.toggle("hide-section")
-    homeSection.classList.toggle("hide-section")
+    sidebar.style.marginLeft = '0';
+    showbtn.style.display = 'none';
+    closebtn.style.display = 'block';
+    
+
+})
+closebtn.addEventListener("click", function(){
+    
+    sidebar.style.marginLeft = '-240px';
+    closebtn.style.display = 'none';
+    showbtn.style.display = 'block';
+      
 })
